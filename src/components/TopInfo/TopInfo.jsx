06bbda9e06e83d6afafed4cdc5112d9f7725a12e
PranayReddyRathna <saipranayrathna007@gmail.com>
2024-09-06 10:23:59 +0530

    added topinfo

---
+++ src/components/TopInfo/TopInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './TopInfo.module.css'
+function TopInfo() {
+    return (
+        <div className={styles.info}>
+            <p> The health and well-being of our patients and their health care team will always be our priority, so we follow the best practices for cleanliness.</p>
+        </div>
+    );
+}
+
+export default TopInfo;
